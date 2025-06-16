abddafad9a26f457554d3df84b30fdc1e0482cbf
Your Name MFlorin13 <mihaila.florin.g@gmail.com>
2025-06-16 19:42:42 +0300

    Change main URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Backend URL (Node.js server)
+  baseURL: "https://rentease-backend-e7a2.onrender.com", // Backend URL (Node.js server)
   headers: {
     "Content-Type": "application/json",
   },
